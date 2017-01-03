c4b4bfa254f1df596bea493faaa7e42a202fc8b1
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-01-03 11:37:25 -0800

    the args need to match for the S4 method

--- R/cppClass/R/NumericVector.R
+++ R/cppClass/R/NumericVector.R
@@ -1,20 +1,6 @@
 setClass("NumericVector", slots = c(data = "numeric"))
 
-
-setMethod("+", signature(a = "NumericVector", b = "NumericVector"), function(a, b)
+setMethod("+", signature(e1 = "NumericVector", e2 = "NumericVector"), function(e1, e2)
 {
     a@data + b@data
 })
-
-class(representation(v="numeric"))
-
-
-setClass("yyy", slots = c(v="numeric"))
-
-setMethod("+", signature(e1 = "yyy", e2 = "yyy"), function (e1, e2)
-          {
-              e1@v + e2@v
-          })
-
-
-y1 <- new("yyy", v = 1)
