335fd0a7c03f7e580b5ec9b945cf1aa06832f70f
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-01-03 11:29:49 -0800

    not understanding how these have different behavior

--- R/cppClass/R/NumericVector.R
+++ R/cppClass/R/NumericVector.R
@@ -1,9 +1,20 @@
 setClass("NumericVector", slots = c(data = "numeric"))
 
 
-signature(a = "NumericVector", b = "NumericVector")
-
-setMethod("+", c(a = "NumericVector", b = "NumericVector"), function(a, b)
+setMethod("+", signature(a = "NumericVector", b = "NumericVector"), function(a, b)
 {
     a@data + b@data
 })
+
+class(representation(v="numeric"))
+
+
+setClass("yyy", slots = c(v="numeric"))
+
+setMethod("+", signature(e1 = "yyy", e2 = "yyy"), function (e1, e2)
+          {
+              e1@v + e2@v
+          })
+
+
+y1 <- new("yyy", v = 1)
