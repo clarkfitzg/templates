8e643cc82e9b742a74d26bbe0dfe1232cf5f4308
Clark Fitzgerald <clarkfitzg@gmail.com>
2017-01-05 12:03:44 -0800

    think I found the right place to look

--- julia/juliaC/l2norm.jl
+++ julia/juliaC/l2norm.jl
@@ -4,8 +4,8 @@ x = [3.0, 4.0]
 
 out = 0.0
 
-libfile = pwd() * "/l2norm.so"
-
+# TODO: Follow this
+# http://docs.julialang.org/en/stable/manual/calling-c-and-fortran-code/#passing-pointers-for-modifying-inputs
 ccall((:c_l2norm, "l2norm"), Void
       , (Ptr{Float64}, Ptr{Int}, Ptr{Float64})
       , Ref(x), Ref(length(x)), Ref(out))
