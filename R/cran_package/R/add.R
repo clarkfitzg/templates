0ced84d3f41d4525d4f9023c45b26491f2bcdce4
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-18 09:27:46 -0700

    now build vignettes

--- R/cran_package/R/add.R
+++ R/cran_package/R/add.R
@@ -1,12 +1,16 @@
-#' Add Two Numbers
-#'
-#' You should just use plus.
-#'
+#' Add together two numbers.
+#' 
+#' This example comes from Hadley Wickham's R packages book: \url{http://r-pkgs.had.co.nz/man.html}.
+#' 
 #' @export
-#' @param x number
-#' @param y number
-#' @returns total another number
-#'
-#' @example
-#' add(1, 2)
-add = function(x, y) x + y
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+add <- function(x, y) {
+  # Illustrate use of different package
+  mypi = MASS::rational(pi)
+  x + y
+}
